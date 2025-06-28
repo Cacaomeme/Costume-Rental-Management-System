8451ef5bff51766c1c5983495631ad7c53578263
shige <shigeyama921@gmail.com>
2025-06-29 01:52:32 +0900

    ログインページ作成

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import gui.LoginFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Set system look and feel for better appearance
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.err.println("Could not set system look and feel: " + e.getMessage());
+        }
+        
+        // Launch the application on EDT (Event Dispatch Thread)
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new LoginFrame().setVisible(true);
+            }
+        });
     }
 }
